eed9b139566435dc190c6e96616436a51de46d8b
kneerunjun <kneerunjun@outlook.com>
2015-11-18 15:27:06 +0530

    working with a service

--- webapp/ng/controllers/loginController.js
+++ webapp/ng/controllers/loginController.js
@@ -1,5 +1,5 @@
 ﻿(function () {
-    var loginController = angular.module("myApp").controller("loginController", function ($scope, $http, $timeout) {
+    var loginController = angular.module("myApp").controller("loginController", function ($scope, $svcUser) {
         console.log("loginController : initiated..");
         $scope.user = {
             username: "",
@@ -9,19 +9,14 @@
             console.log("loginController: we are now ready to login..");
             if ($scope.user.username!="" && $scope.user.password!="") {
                 console.log("loginController: bingo ! we are now ready to hit the server..");
-                //var url = "http://vpunplepun2-01:8080/webapi/users"; //this is imaginary url for the api
-                //$http.get(url).then(function (response) {
-                //    //suucess
-
-                //}, function (response) {
-                //    //failure
-                //    console.log(response.ErrorMessage);
-                //}); //this is on a different thread
-                console.log("we are waiting for the server to respond!, but wait a minute! there aint any server :)");
-
-                $timeout(function () {
-                    console.log("we have received the response fro the server");
-                }, 2500)
+                $svcUser.authenticate($scope.user.username, $scope.user.password)
+                    .then(function (data) {
+                        console.log("logincontroller: we have authenticated th user");
+                        console.log(data);
+                }, function (data) {
+                    console.log("logincontroller : the user has failed authentication");
+                    console.log(data.ErrorMessage);
+                });
                 //this is on the main thread
                 console.log("you have no idea and control on when this statement would run");
             }
---
+++ webapp/ng/services/svcUser.js
@@ -0,0 +1,35 @@
+﻿(function () {
+    var $svcUser = angular.module("myApp").service("$svcUser", function ($http, $timeout, $q) {
+        this.authenticate = function (username, password) {
+            var deferred = $q.defer();
+            //var url = "http://vpunplepun2-01:8080/webapi/users"; //this is imaginary url for the api
+            //$http.get(url).then(function (response) {
+            //    //suucess
+
+            //}, function (response) {
+            //    //failure
+            //    console.log(response.ErrorMessage);
+            //}); //this is on a different thread
+            console.log("we are waiting for the server to respond!, but wait a minute! there aint any server :)");
+
+            $timeout(function () {
+                if (username=="niranjan_awati" && password =="41993") {
+                    console.log("$svcUser/authenticate: the user has been authenticated");
+                    deferred.resolve({
+                        email: "niranjan_awati",
+                        empno: 41993,
+                        alias: "Niranjan V Awati",
+                        location: { title: "pune", code: "PUN" },
+                        role: {title:"lecturer"}
+                    });
+                }
+                else {
+                    console.log("$svcUsr/authenticate: the user has failed authentication");
+                    deferred.reject({ErrorMessage:"the user has failed authentication!"});
+                }
+            }, 2500)
+
+            return deferred.promise;
+        }
+    })
+})();
