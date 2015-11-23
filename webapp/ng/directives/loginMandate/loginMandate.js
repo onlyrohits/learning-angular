7f8a3dc2875711e2690361d3e69cc77590b84704
kneerunjun <kneerunjun@outlook.com>
2015-11-23 16:27:17 +0530

    setting up watches in directives

--- webapp/ng/directives/loginMandate/loginMandate.js
+++ webapp/ng/directives/loginMandate/loginMandate.js
@@ -2,26 +2,42 @@
     var loginMandate = angular.module("myApp").directive("loginMandate", function () {
         return {
             restrict: "A",
-            scope: {
-                fnGetUser: "=",
-            },
+            scope: false,
             compile: function (telem, tattrs) {
                 console.log("we are inside the compile function");
 
                 return{
                     pre: function (scope, elem, attrs) {
-                        //var attrValue = $(elem[0]).attr("login-mandate");
-                        //console.log(attrValue);
-                        //var fn = scope.$eval($(elem[0]).attr("login-mandate"));
-                        //console.log(fn);
+                       //process here
                     },
                     post: function (scope, elem, attrs) {
-                       
+                        scope.getuser();
                     }
                 }
             },
             controller: function ($scope, $location, $routeParams) {
-                $scope.fnGetUser();
+                $scope.getuser = function () {
+                    console.log("loginMandate: we are now ready to hydrate the user");
+                    $scope.user = {
+                        empno: 41993,
+                        email: "niranjan_Awati"
+                    };
+                   
+                }
+            }
+        }
+    });
+    var watchingUser = angular.module("myApp").directive("watchingUser", function () {
+        return {
+            restrict: "E",
+            scope: false,
+          
+            controller: function ($scope) {
+                $scope.$watch("user", function (newValue, oldValue) {
+                    if (newValue != null) {
+                        console.log("watchingUser:the user object is received from the server");
+                    }
+                })
             }
         }
     });
