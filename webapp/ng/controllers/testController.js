de9a1686a4e2ee78b1518ae9ff24cef6f789727a
kneerunjun <kneerunjun@outlook.com>
2015-12-11 05:46:30 +0530

    reset for december mysore lectures

--- webapp/ng/controllers/testController.js
+++
@@ -1,6 +0,0 @@
-﻿(function () {
-    var testController = angular.module("myApp").controller("testController", function ($scope, $routeParams, $svcUser, $location) {
-        console.log("the empno you are looking for as per the url :" + $routeParams.empno);
-        $scope.delgGetUser = $svcUser.ofEmpno;
-    })
-})();
