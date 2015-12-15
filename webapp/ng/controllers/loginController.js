6f140a73396b4e15f032f082bc2e1cc04051c0af
kneerunjun <kneerunjun@outlook.com>
2015-12-15 15:35:09 +0530

    connected controller to login

---
+++ webapp/ng/controllers/loginController.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    var loginController = angular.module("myApp").controller("loginController", function ($scope) {
+        $scope.user = {
+            username: "",
+            password:""
+        }
+        $scope.login = function () {
+            console.log("we have started the process of login");
+            console.log($scope.user);
+        }
+    })
+})();
--- webapp/ng/myApp.js
+++ webapp/ng/myApp.js
@@ -2,8 +2,8 @@
     var myApp = angular.module("myApp", ["ngRoute"])
     .config(function ($routeProvider) {
         $routeProvider
-        .when("/", { templateUrl: "/ng/views/login.html", controller: "" })
-        .when("/login", { templateUrl: "/ng/views/login.html", controller: "" })
+        .when("/", { templateUrl: "/ng/views/login.html", controller: "loginController" })
+        .when("/login", { templateUrl: "/ng/views/login.html", controller: "loginController" })
         .when("/reportees", { templateUrl: "/ng/views/reportees.html", controller: "" })
         .when("/accconfig", { templateUrl: "/ng/views/accountconfig.html", controller: "" })
         .otherwise({redirectTo:"/"});
