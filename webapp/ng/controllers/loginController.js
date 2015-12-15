4fe0e574d4c186efad9033f626f050964c39598c
kneerunjun <kneerunjun@outlook.com>
2015-12-15 17:16:14 +0530

    invalidated styling applied with ngclass demo

--- webapp/ng/controllers/loginController.js
+++ webapp/ng/controllers/loginController.js
@@ -4,6 +4,10 @@
             //verifies that the username and the password are filled up by the user
             return $scope.user.username != "" && $scope.user.password != "" ? true : false;
         }
+        var endBusyAnimation = function () {
+            console.log("the animation is stopped and the user can see the login screen again");
+            $scope.showAnimation = false;
+        }
         var runBusyAnimation = function () {
             console.log("we pretend that there is an animation running right now..");
             $scope.showAnimation = true;
@@ -30,15 +34,13 @@
             }, 2000)
             console.log("we are not sure when this line would run , before or after the server response");
         }
-        var endBusyAnimation = function () {
-            console.log("the animation is stopped and the user can see the login screen again");
-            $scope.showAnimation = false;
-        }
+        
         var invalidate = function () {
             //all that needs to be done before the user sees the error message
+            $scope.invalidated = true;
         }
         $scope.showAnimation = false;
-
+        $scope.invalidated = false;
         $scope.user = {
             username: "",
             password:""
