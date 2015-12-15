b515723ed45e68c28745820ec7e7343572423fb3
kneerunjun <kneerunjun@outlook.com>
2015-12-15 14:52:08 +0530

    demo for the router and routeprovider service

--- webapp/ng/myApp.js
+++ webapp/ng/myApp.js
@@ -1,4 +1,12 @@
 ﻿(function myfunction() {
-    var myApp = angular.module("myApp", []);
+    var myApp = angular.module("myApp", ["ngRoute"])
+    .config(function ($routeProvider) {
+        $routeProvider
+        .when("/", { templateUrl: "/ng/views/login.html", controller: "" })
+        .when("/login", { templateUrl: "/ng/views/login.html", controller: "" })
+        .when("/reportees", { templateUrl: "/ng/views/reportees.html", controller: "" })
+        .when("/accconfig", { templateUrl: "/ng/views/accountconfig.html", controller: "" })
+        .otherwise({redirectTo:"/"});
+    });
 
 })(); //IIFE - immediately invoked function expression
