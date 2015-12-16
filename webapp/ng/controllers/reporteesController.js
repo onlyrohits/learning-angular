501d15eb87a62e0379eb1fc3c627b433a4d8536b
kneerunjun <kneerunjun@outlook.com>
2015-12-16 16:54:57 +0530

    demo for filters on enumerations

--- webapp/ng/controllers/reporteesController.js
+++ webapp/ng/controllers/reporteesController.js
@@ -1,5 +1,6 @@
 ﻿(function () {
     var reporteesController = angular.module("myApp").controller("reporteesController", function ($scope, srvEmployee) {
+        $scope.phrase = "";
         $scope.reportees = [];
         srvEmployee.getAll().then(function (data) {
             $scope.reportees = data;
--- webapp/ng/myApp.js
+++ webapp/ng/myApp.js
@@ -7,6 +7,23 @@
         .when("/reportees", { templateUrl: "/ng/views/reportees.html", controller: "reporteesController" })
         .when("/accconfig", { templateUrl: "/ng/views/accountconfig.html", controller: "" })
         .otherwise({redirectTo:"/"});
+    })
+    .filter("likely", function () {
+        return function (input, phrase) {
+            if (phrase!="") {
+                var filteredResult = [];
+                $.each(input, function (index, elem) {
+                    if (elem.email.indexOf(phrase) >= 0) {
+                        filteredResult.push(elem);
+                    }
+                });
+                return filteredResult;
+            }
+            else {
+                return input
+            }
+        }
     });
+    
 
 })(); //IIFE - immediately invoked function expression
