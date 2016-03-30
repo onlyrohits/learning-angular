50bf9715480f7a16c2ba8b731a3b14341c3c3392
Niranjan Awati <kneerunjun@live.com>
2016-03-30 14:39:03 +0530

    demo for routes included

---
+++ ng/myApp.js
@@ -0,0 +1,16 @@
+﻿//iife;
+(function () {
+
+    var myApp = angular.module("myApp", ["ngRoute"])
+    .config(function ($routeProvider) {
+        $routeProvider
+        .when("/", { templateUrl: "ng/views/login.html", controller: "" })
+        .when("/login", { templateUrl: "ng/views/login.html", controller: "" })
+        .when("/employees", { templateUrl: "ng/views/employees.html", controller: "" })
+    })
+    .controller("mainController", function ($scope) {
+
+    });
+
+})();
+    
