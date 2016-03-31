4a7b5a1033e9d069e84c7e5befb721192c83f3c2
Niranjan Awati <kneerunjun@live.com>
2016-03-31 14:52:14 +0530

    simple password based authentication demo

--- ng/controllers/loginController.js
+++ ng/controllers/loginController.js
@@ -1,11 +1,29 @@
 ﻿(function () {
-    var loginController = angular.module("myNgApp").controller("loginController", function ($scope) {
+    var loginController = angular.module("myNgApp").controller("loginController", function ($scope, $location, $http) {
         $scope.username = "";
         $scope.password = "";
         $scope.login = function () {
-            console.debug("we are ready to login the user...");
-            console.debug($scope.username);
-            console.debug($scope.password);
+            /*this is where we do the login activity
+             * 
+             */
+            var url = "http://vpunplepun2-01:8085/data/employees.json";
+            $http.get(url).then(function (response) {
+                var employees = response.data.employees;
+                var emp = employees.filter(function (el) {
+                    return el.email == $scope.username;
+                })[0];
+                if (emp.account.password ==$scope.password) {
+                    console.info("authentication was successful")
+                    $location.url("/employees");
+                }
+                else {
+                    console.error("check username and password combination");
+                }
+            }, function (response) {
+
+            });
+
+           
         }
     })
 
