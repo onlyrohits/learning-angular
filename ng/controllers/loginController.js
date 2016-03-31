94de0a347bfc0fd8e68ac62e24c43c6d3fbca21d
Niranjan Awati <kneerunjun@live.com>
2016-03-31 15:22:39 +0530

    simple service demo

--- ng/controllers/loginController.js
+++ ng/controllers/loginController.js
@@ -1,28 +1,25 @@
 ﻿(function () {
-    var loginController = angular.module("myNgApp").controller("loginController", function ($scope, $location, $http) {
+    var loginController = angular.module("myNgApp").controller("loginController", function ($scope, $location, svcEmployee) {
         $scope.username = "";
         $scope.password = "";
         $scope.login = function () {
-            /*this is where we do the login activity
-             * 
-             */
-            var url = "http://vpunplepun2-01:8085/data/employees.json";
-            $http.get(url).then(function (response) {
-                var employees = response.data.employees;
-                var emp = employees.filter(function (el) {
+          
+            svcEmployee.GETOfEmail($scope.username).then(function (data) {
+                /*this is deferred.resolve*/
+                var emp = data.filter(function (el) {
                     return el.email == $scope.username;
                 })[0];
-                if (emp.account.password ==$scope.password) {
-                    console.info("authentication was successful")
+                if (emp.account.password == $scope.password) {
+                    console.info("authentication was successful");
                     $location.url("/employees");
                 }
                 else {
                     console.error("check username and password combination");
                 }
-            }, function (response) {
-
+            }, function (data) {
+                /*this is deferred.reject*/
             });
-
+           
            
         }
     })
---
+++ ng/services/svcEmployee.js
@@ -0,0 +1,18 @@
+﻿(function () {
+
+    var svcEmployee = angular.module("myNgApp").service("svcEmployee", function ($http, $q) {
+        this.GETOfEmail = function (email) {
+            var deferred = $q.defer();
+            var url = "http://vpunplepun2-01:8085/data/employees.json";
+            $http.get(url).then(function (response) {
+                deferred.resolve(response.data.employees)
+              
+            }, function (response) {
+                deferred.reject({});
+            }); 
+            return deferred.promise;
+
+         }
+    })
+
+})();
