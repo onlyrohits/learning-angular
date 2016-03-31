87f0b3882755926523f1eb7be411f624388c4eb6
Niranjan Awati <kneerunjun@live.com>
2016-03-31 14:26:16 +0530

    ..

---
+++ ng/controllers/loginController.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    var loginController = angular.module("myNgApp").controller("loginController", function ($scope) {
+        $scope.username = "";
+        $scope.password = "";
+        $scope.login = function () {
+            console.debug("we are ready to login the user...");
+            console.debug($scope.username);
+            console.debug($scope.password);
+        }
+    })
+
+
+})();
