ac7b5a3a65e829951841eedfcbec2488fa08e2f8
Niranjan Awati <kneerunjun@live.com>
2016-03-31 14:22:47 +0530

    bulding the login screen

--- ng/myNgApp.js
+++ ng/myNgApp.js
@@ -2,7 +2,10 @@
 
     var myNgApp = angular.module("myNgApp", ["ngRoute"])
     .config(function ($routeProvider) {
-        $routeProvider.when("/", {templateUrl:"ng/views/login.html", controller:""})
+        $routeProvider
+            .when("/", { templateUrl: "ng/views/login.html", controller: "loginController" })
+            .when("/login", { templateUrl: "ng/views/login.html", controller: "loginController" })
+            .when("/employees", {templateUrl:"ng/views/employees.html", controller:""})
     })
 
 })();
