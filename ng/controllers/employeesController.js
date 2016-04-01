e3d77d04037c5c904d32f1edc3fe7a9101317f23
Niranjan Awati <kneerunjun@live.com>
2016-04-01 14:54:56 +0530

    extending the view model and ng-class

--- ng/controllers/employeesController.js
+++ ng/controllers/employeesController.js
@@ -8,11 +8,20 @@
         }
         else {
             svcEmployee.GETAll().then(function (data) {
+                data.forEach(function (el) {
+                    el.picked = false;
+                })
                 $scope.employees = data;
                 console.table($scope.employees);
             }, function (data) {
                 /*not worried about this error response now*/
             })
+        }
+        $scope.save = function () {
+            var itemsChanged = $scope.employees.filter(function (el) {
+                return el.picked == true;
+            });
+            console.table(itemsChanged);
         }
     })
 
