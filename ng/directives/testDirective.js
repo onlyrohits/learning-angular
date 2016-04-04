ab81d08b88c23b21bdc6e71a24e43b191889148c
Niranjan Awati <kneerunjun@live.com>
2016-04-04 15:50:47 +0530

    transclusion basic demo

--- ng/directives/testDirective.js
+++ ng/directives/testDirective.js
@@ -12,5 +12,23 @@
             templateUrl:"ng/templates/testDirective.html"
         }
     })
+    var levelOne = angular.module("myNgApp").directive("levelOne", function () {
+        return {
+            restrict: "E",
+            template: "<div> <p>this is the text from level-one</p> <ng-transclude></ng-transclude> </div>",
+            controller: function ($scope) {
 
+            },
+            transclude:true
+        }
+    });
+    var levelTwo = angular.module("myNgApp").directive("levelTwo", function () {
+        return {
+            restrict: "E",
+            template: "<div> <p>this is the text from level-two</p></div>",
+            controller: function ($scope) {
+
+            }
+        }
+    })
 })();
