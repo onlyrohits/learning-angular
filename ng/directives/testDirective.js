950ef74778399fba99e6785079058024b0f67634
Niranjan Awati <kneerunjun@live.com>
2016-04-07 14:36:01 +0530

    compilation sequence in transcluding directives

--- ng/directives/testDirective.js
+++ ng/directives/testDirective.js
@@ -17,19 +17,64 @@
             restrict: "E",
             template: "<div> <p>this is the text from level-one</p> <ng-transclude></ng-transclude> </div>",
             controller: function ($scope) {
-
+                console.info("level-one:controller");
             },
-            transclude:true
+            transclude: true,
+            compile: function (tElem, tAttrs) {
+                console.info("level-one:compiler");
+                return {
+                    pre: function (scope, elem, attrs, ctroller) {
+                        console.info("level-one:pre");
+
+                    },
+                    post: function (scope, elem, attrs, ctroller) {
+                        console.info("level-one:post");
+                    },
+                }
+            }
         }
     });
     var levelTwo = angular.module("myNgApp").directive("levelTwo", function () {
         return {
             restrict: "E",
-            template: "<div> <p>this is the text from level-two</p></div>",
+            template: "<div> <p>this is the text from level-two</p><ng-transclude></ng-transclude></div>",
             controller: function ($scope) {
+                console.info("level-two:controller");
+            },
+            transclude: true,
+            compile: function (tElem, tAttrs) {
+                console.info("level-two:compiler");
+                return {
+                    pre: function (scope, elem, attrs, ctroller) {
+                        console.info("level-two:pre");
 
+                    },
+                    post: function (scope, elem, attrs, ctroller) {
+                        console.info("level-two:post");
+                    },
+                }
             }
         }
     })
-   
+    var levelThree = angular.module("myNgApp").directive("levelThree", function () {
+        return {
+            restrict: "E",
+            template: "<div></div>",
+            controller: function ($scope) {
+                console.info("level-three:controller");
+            },
+            compile: function (tElem, tAttrs) {
+                console.info("level-three:compiler");
+                return {
+                    pre: function (scope, elem, attrs, ctroller) {
+                        console.info("level-three:pre");
+
+                    },
+                    post: function (scope, elem, attrs, ctroller) {
+                        console.info("level-three:post");
+                    },
+                }
+            }
+        }
+    })
 })();
